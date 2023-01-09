6af79bc3dc8f89b2c33f646592bb7cb79124baf9
FaisalABR <faisalabubakar92@gmail.com>
2023-01-09 18:22:25 +0700

    second commit

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
-const connectDB = require('./config/db');
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const cors = require("cors");
 
-
-const todos = require('./routes/todos'); 
-const users = require('./routes/users'); 
+const todos = require("./routes/todos");
+const users = require("./routes/users");
 const PORT = process.env.PORT;
 
 const app = express();
 
 connectDB();
-
+app.use(cors());
+app.use(express);
 app.use(bodyParser.json());
 
 app.use("/api/v1/todos", todos);
 app.use("/api/v1/user", users);
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
-app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+app.all("*", (req, res) =>
+  res.send("You've tried reaching a route that doesn't exist.")
+);
 
-app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
